b9c449a86ea2942699b20d3ef4a52c2b259a581a
Matias Alvez <matias2018alvez@gmail.com>
2022-11-17 14:12:43 -0300

    version 0.2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import "./App.css";
-import Title from "./componets/title";
 
 function App() {
-  const categories = ["Home", "Food", "Wather"];
   return (
     <div className="App">
-      <header className="App-header">
-        <Title title='Mercado Libre'categories={categories}/>
-      </header>
     </div>
   );
 }
